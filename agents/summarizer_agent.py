f88df2cda3c7e157c92db64e3279d756e6d909d4
muzza <mmuzzammil19@gmail.com>
2026-03-18 16:36:56 -0500

    fixing files

--- agents/summarizer_agent.py
+++ agents/summarizer_agent.py
@@ -1,11 +1,11 @@
 #  2. Summarizes each paper.
 
-# class VectorDB:
-#     def __init__(self, embedding_dim=384, use_cosine: bool = True):
+class VectorDB:
+    def __init__(self, embedding_dim=384, use_cosine: bool = True):
  
-#         self.embedding_dim = embedding_dim
-#         self.use_cosine = use_cosine
+        self.embedding_dim = embedding_dim
+        self.use_cosine = use_cosine
 
-#         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
-#         self.metadata = []
-#         self.id_counter = 0 
+        self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
+        self.metadata = []
+        self.id_counter = 0 
