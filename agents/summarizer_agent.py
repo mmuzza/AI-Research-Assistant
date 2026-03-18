aee2dcb7fdae5e880be030b3fb1e96e24201ccd7
muzza <mmuzzammil19@gmail.com>
2026-03-18 16:38:30 -0500

    testing push

--- agents/summarizer_agent.py
+++ agents/summarizer_agent.py
@@ -9,3 +9,38 @@ class VectorDB:
         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
         self.metadata = []
         self.id_counter = 0 
+
+# class VectorDB:
+#     def __init__(self, embedding_dim=384, use_cosine: bool = True):
+ 
+#         self.embedding_dim = embedding_dim
+#         self.use_cosine = use_cosine
+
+#         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
+#         self.metadata = []
+#         self.id_counter = 0 
+# class VectorDB:
+#     def __init__(self, embedding_dim=384, use_cosine: bool = True):
+ 
+#         self.embedding_dim = embedding_dim
+#         self.use_cosine = use_cosine
+
+#         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
+#         self.metadata = []
+#         self.id_counter = 0 # class VectorDB:
+#     def __init__(self, embedding_dim=384, use_cosine: bool = True):
+ 
+#         self.embedding_dim = embedding_dim
+#         self.use_cosine = use_cosine
+
+#         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
+#         self.metadata = []
+#         self.id_counter = 0 # class VectorDB:
+#     def __init__(self, embedding_dim=384, use_cosine: bool = True):
+ 
+#         self.embedding_dim = embedding_dim
+#         self.use_cosine = use_cosine
+
+#         self.index = faiss.IndexIDMap(faiss.IndexFlatL2(embedding_dim))
+#         self.metadata = []
+#         self.id_counter = 0 
